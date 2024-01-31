8c1f3a822295009111f057d7e5d8825fed89575b
Marvin Aaron Panerio <marvinpanerio@Marvins-MacBook-Pro.local>
2024-01-31 13:24:02 +0800

    modefied index.html and addinng some js in the assets/js/global.js

---
+++ assets/js/global-header.js
@@ -0,0 +1,19 @@
+// class GlobalHeader extends HTMLElement {
+//     ConnectedCallBack () {
+//         this.innerHTML = '
+//         <nav class="main-navigation-menu">
+//             <ul class="main-navigation-links">
+//                 <li><a href="#" onclick="setActive(this)">About Me</a></li>
+//                 <li><a href="#" onclick="setActive(this)">Services</a></li>
+//                 <li><a href="#" onclick="setActive(this)">Portfolio</a></li>
+//                 <li><a href="#" onclick="setActive(this)">Testimonials</a></li>
+//                 <li><a href="#" onclick="setActive(this)">Blog</a></li>
+//                 <li><a href="#" onclick="setActive(this)">Contact Us</a></li>
+//                 <li class="cta-button"><a href="#" class="main-navigation-cta-button">Hire Me</a></li>
+//             </ul>
+//         </nav>
+//         '
+//     }
+// }
+
+// customElement.define('global-header', GlobalHeader)
