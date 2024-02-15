22b1ea84e1076ddaabe4c538658fd1430e736ffc
Your Name <you@example.com>
2024-02-15 22:31:39 +0800

    modified hover stroke

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,1 +1,6 @@
 document.body.insertAdjacentHTML("afterbegin", '<div class="speech-bubble"></div>');
+
+function hoverH1() {
+    var h1 = document.querySelector('.hover-stroke');
+    h1.classList.add('hover-stroke-hover');
+}
